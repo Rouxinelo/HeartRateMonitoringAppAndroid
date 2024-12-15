2ce7ffb0348d4f78a4a6a49b4bbeac0cafe64228
João Rouxinol <m54076@alunos.uevora.pt>
2024-12-15 19:16:46 +0000

    Small changes to radio button

--- HeartRateMonitoringApp/app/src/main/java/components/buttons/CustomRadioButton.kt
+++ HeartRateMonitoringApp/app/src/main/java/components/buttons/CustomRadioButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -36,7 +38,10 @@ fun CustomRadioButton(
                 interactionSource = remember { MutableInteractionSource() },
                 onClick = { onSelectionChange(!isSelected) },
             )
-            .padding(8.dp) // Optional padding to space out the button
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+
     ) {
         Text(
             text = text,
