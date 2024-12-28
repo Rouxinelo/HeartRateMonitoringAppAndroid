773d0e163ebc0f11e33ac0703c3be10884387a21
João Rouxinol <m54076@alunos.uevora.pt>
2024-12-28 17:56:50 +0000

    create circular avatar

---
+++ HeartRateMonitoringApp/app/src/main/java/components/others/CircularAvatar.kt
@@ -0,0 +1,43 @@
+package components.others
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CircularAvatar(
+    backgroundColor: Color,
+    textColor: Color,
+    text: String
+) {
+    Box(
+        modifier = Modifier
+            .size(75.dp)
+            .shadow(10.dp, CircleShape)
+            .background(backgroundColor, CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAvatar() {
+    CircularAvatar(backgroundColor = Color.Red, textColor = Color.White, text = "JR")
+}
