37365869ccfa545a963305c99138da73aac96f8e
João Rouxinol <m54076@alunos.uevora.pt>
2024-12-28 21:21:01 +0000

    remove unnecessary imports

--- HeartRateMonitoringApp/app/src/main/java/components/sections/SessionSection.kt
+++ HeartRateMonitoringApp/app/src/main/java/components/sections/SessionSection.kt
@@ -12,9 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -29,6 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 import com.example.heartratemonitoringapp.R
 
 @Composable
