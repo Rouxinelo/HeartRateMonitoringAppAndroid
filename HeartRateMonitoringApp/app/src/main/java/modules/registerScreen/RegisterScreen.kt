137e2aeca0c983875a08da2ea9b7b730c7f260cf
João Rouxinol <m54076@alunos.uevora.pt>
2025-01-01 22:37:48 +0000

    create recovery code screen

--- HeartRateMonitoringApp/app/src/main/java/modules/registerScreen/RegisterScreen.kt
+++ HeartRateMonitoringApp/app/src/main/java/modules/registerScreen/RegisterScreen.kt
@@ -17,13 +17,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import components.sections.Gender
 import components.sections.RegisterScreenField
@@ -53,10 +50,9 @@ fun RegisterUserScreen(
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth() // Change to fillMaxWidth instead of fillMaxSize
+                .fillMaxWidth()
                 .padding(horizontal = 16.dp)
         ) {
-            // Top App Bar with Title
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
